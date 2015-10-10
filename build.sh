aa08ada4e43fffb6cdd9fd8dd683bd80640f5f83
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-10-11 05:06:49 +0900

    テストを合格してからインストールするようにスクリプトを改造。

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # 依存性解決のgo get
 go get github.com/bitly/go-simplejson
+# テスト
+go test ./...
+if [[ $? -ne 0 ]] ; then
+  echo 'Test failed and Build failed!'
+  exit 1
+fi
 # ビルド・インストール
 go install
