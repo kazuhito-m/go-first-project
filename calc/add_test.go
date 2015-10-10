df4b01a133ff74e899686b6d26bb08c8d985abcd
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-10-11 04:26:55 +0900

    「足し算・引き算するパッケージ」を作り、テストもついか

---
+++ calc/add_test.go
@@ -0,0 +1,22 @@
+package calc
+
+import "testing"
+
+
+// 引き算のテストその１
+func TestAdd(t *testing.T) {
+  const x, y, expected = 1, 2, 3
+  result := Add(x, y)
+  if result != expected {
+    t.Errorf("Add(%v, %v) = %v, want: %v", x, y, result, expected)
+  }
+}
+
+// 引き算のテストその１
+func TestAdd2(t *testing.T) {
+  const x, y, expected = 2, 4, 6
+  result := Add(x, y)
+  if result != expected {
+    t.Errorf("Add(%v, %v) = %v, want: %v", x, y, result, expected)
+  }
+}
---
+++ calc/sub.go
@@ -0,0 +1,5 @@
+package calc
+
+func Sub(x int64, y int64) int64 {
+  return x - y
+}
