547d614d8d4dce4dca0d60b75a58b815f8eb1699
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-10-11 05:57:23 +0900

    パッケージ分けたものを、大本のプログラムであるmainで使うように変更。

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/bitly/go-simplejson"
+	"github.com/kazuhito-m/go-first-project/calc"
 )
 
 func main() {
@@ -12,4 +13,9 @@ func main() {
 	json.Set("message", "Hello, World!")
 	b, _ := json.EncodePretty()
 	fmt.Printf("%s\n", b)
+	// 計算の例
+	const v1 , v2 = 1 , 2
+	fmt.Printf("Add(%v, %v) = %v\n", v1, v2, calc.Add(v1, v2))
+	fmt.Printf("Sub(%v, %v) = %v\n", v2, v1, calc.Sub(v2, v1))
+
 }
