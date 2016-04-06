8bde1a37ea360b1a33ba695c1930487e4ffaae8d
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-04-07 01:27:06 +0900

    コードフォーマットを全体にかけた。

--- calc/sub_test.go
+++ calc/sub_test.go
@@ -2,21 +2,20 @@ package calc
 
 import "testing"
 
-
 // 引き算のテストその１
 func TestSub(t *testing.T) {
-  const x, y, expected = 3, 2, 1
-  result := Sub(x, y)
-  if result != expected {
-    t.Errorf("sub(%v, %v) = %v, want: %v", x, y, result, expected)
-  }
+	const x, y, expected = 3, 2, 1
+	result := Sub(x, y)
+	if result != expected {
+		t.Errorf("sub(%v, %v) = %v, want: %v", x, y, result, expected)
+	}
 }
 
 // 引き算のテストその１
 func TestSub2(t *testing.T) {
-  const x, y, expected = 6, 4, 2
-  result := Sub(x, y)
-  if result != expected {
-    t.Errorf("sub(%v, %v) = %v, want: %v", x, y, result, expected)
-  }
+	const x, y, expected = 6, 4, 2
+	result := Sub(x, y)
+	if result != expected {
+		t.Errorf("sub(%v, %v) = %v, want: %v", x, y, result, expected)
+	}
 }
--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	b, _ := json.EncodePretty()
 	fmt.Printf("%s\n", b)
 	// 計算の例
-	const v1 , v2 = 1 , 2
+	const v1, v2 = 1, 2
 	fmt.Printf("Add(%v, %v) = %v\n", v1, v2, calc.Add(v1, v2))
 	fmt.Printf("Sub(%v, %v) = %v\n", v2, v1, calc.Sub(v2, v1))
 
