c428436c034cbb085684a23aa20594f1e3ffcd59
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-04-07 01:36:42 +0900

    変数名が納得いかなかったので、微調整。

--- calc/sub_test.go
+++ calc/sub_test.go
@@ -5,17 +5,17 @@ import "testing"
 // 引き算のテストその１
 func TestSub(t *testing.T) {
 	const x, y, expected = 3, 2, 1
-	result := Sub(x, y)
-	if result != expected {
-		t.Errorf("sub(%v, %v) = %v, want: %v", x, y, result, expected)
+	actual := Sub(x, y)
+	if actual != expected {
+		t.Errorf("sub(%v, %v) = %v, want: %v", x, y, actual, expected)
 	}
 }
 
 // 引き算のテストその１
 func TestSub2(t *testing.T) {
 	const x, y, expected = 6, 4, 2
-	result := Sub(x, y)
-	if result != expected {
-		t.Errorf("sub(%v, %v) = %v, want: %v", x, y, result, expected)
+	actual := Sub(x, y)
+	if actual != expected {
+		t.Errorf("sub(%v, %v) = %v, want: %v", x, y, actual, expected)
 	}
 }
